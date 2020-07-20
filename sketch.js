576dc694e33b57224ecbce07c8454fbc697a4405
VineethEmmanuel <vineethemmanuel75@gmail.com>
2020-07-20 19:59:23 +0530

    I added My Name

--- sketch.js
+++ sketch.js
@@ -40,6 +40,8 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    textSize(25);
+    text("Vineeth's Angry Bird Game",200,200);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
